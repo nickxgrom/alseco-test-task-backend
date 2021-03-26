78556b384ccae652c821b46bb14c20916047d6e7
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-03-26 22:10:44 +0600

    Add createEmployee post-method

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,11 @@
 const express = require("express"),
-    router = express.Router()
+    router = express.Router(),
+    EmployeeService = require("./services/EmployeeService"),
+    catchError = require("./util")
+
+router.post("/employees", catchError(async (req, res, next) =>  {
+    await EmployeeService.createEmployee(req.body.firstName, req.body.secondName, req.body.patronymic)
+    res.sendStatus(200)
+}))
 
 module.exports = router
