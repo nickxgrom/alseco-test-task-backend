b8363b194b9d8aa18f0f80d666a1dae1f374fba2
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-03-26 23:40:10 +0600

    Add createEmployeeMaterialValue post-method

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,16 @@
 const express = require("express"),
     router = express.Router(),
     EmployeeService = require("./services/EmployeeService"),
+    MaterialValueService = require("./services/MaterialValueService")
     catchError = require("./util")
 
 router.post("/employees", catchError(async (req, res, next) =>  {
     await EmployeeService.createEmployee(req.body.firstName, req.body.secondName, req.body.patronymic)
+    res.sendStatus(200)
+}))
+
+router.post("/employees/:id", catchError(async (req, res, next) => {
+    await MaterialValueService.createEmployeeMaterialValue(req.params.id, req.body.materialValueName, req.body.materialValuePrice)
     res.sendStatus(200)
 }))
 
---
+++ src/services/MaterialValueService.js
@@ -0,0 +1,28 @@
+const MaterialValue = require("../models/MaterialValues"),
+    Employee = require("../models/Employees"),
+    ServiceError = require("../error")
+
+const isEmployeeExist = async (id) => {
+    return !!(await Employee.findByPk(id))
+},
+    isMaterialValueExist = async (id) => {
+    return !!(await MaterialValue.findOne({
+        where: { employeeId: id }
+    }))
+    }
+
+module.exports = {
+    createEmployeeMaterialValue: async (id, materialValueName, materialValuePrice) => {
+        if (await isEmployeeExist(id)) {
+            if (await isMaterialValueExist(id))
+                throw new ServiceError(409, "Employee with given id already has this material value");
+            await MaterialValue.create({
+                name: materialValueName,
+                price: materialValuePrice,
+                employeeId: id
+            })
+        } else {
+            throw new ServiceError(404, "Employee with given id is not found")
+        }
+    }
+}
