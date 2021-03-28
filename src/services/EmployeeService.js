5aa84bc67f8ff155c5a8c46d20c7fbca93915690
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-03-28 17:10:47 +0600

    Refactor createEmployee methods

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -2,26 +2,27 @@ const Employee = require("../models/Employees"),
      ServiceError = require("../error")
 
 const getEmployeeOrThrowError = async (employeeId) => {
-    const employee = await Employee.findByPk(employeeId)
+        const employee = await Employee.findByPk(employeeId)
 
-    if (!employee) {
-        throw new ServiceError(404, "Employee doesn't exist")
-    }
-    return employee
-};
-
-module.exports = {
-    createEmployee: async (firstName, secondName, patronymic) => {
-        const hasEmployee = await Employee.findOne({
+        if (!employee) {
+            throw new ServiceError(404, "Employee doesn't exist")
+        }
+        return employee
+    },
+    throwErrorIfExist = async (firstName, secondName, patronymic) => {
+        const isEmployeeExist = await Employee.findOne({
             where: {
                 firstName, secondName, patronymic
             }
         })
-
-        if (hasEmployee) {
+        if (isEmployeeExist) {
             throw new ServiceError(409, "Employee already exist")
         }
+    }
 
+module.exports = {
+    createEmployee: async (firstName, secondName, patronymic) => {
+        await throwErrorIfExist(firstName, secondName, patronymic)
         await Employee.create({
             firstName,
             secondName,
