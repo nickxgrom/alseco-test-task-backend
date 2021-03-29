32bdb12448f98f7f7989dc80327315edd223f5aa
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-03-29 19:46:15 +0600

    Fix `mysql out of range` error

--- src/db.js
+++ src/db.js
@@ -2,5 +2,6 @@ const { Sequelize } = require("sequelize")
 
 module.exports = new Sequelize('alseco', 'root', 'root', {
     host: 'localhost',
-    dialect: "mysql"
+    dialect: "mysql",
+    logging: false
 })
